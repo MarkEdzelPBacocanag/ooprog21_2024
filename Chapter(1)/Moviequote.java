d844b1db2b34601c4bfdffcb724d42de6dcd7039
Mark Edzel P. Bacocanag <markbacocanag123@gmail.com>
2024-08-14 20:46:20 +0800

    Chapter1 activities

---
+++ Chapter(1)/Moviequote.java
@@ -0,0 +1,6 @@
+public class Moviequote {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Earth.");
+        System.out.println("-Will Smith as Captain Steven Hiller in Independence Day (1996)");
+    }
+}
---
+++ Chapter(1)/Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int row = 10;
+        int j;
+        for(int i = 0; i < row; i++){
+
+            for (j = row - i - 1 ; j >= 1; j--){ 
+            System.out.println(" ");
+            for (j = 0; j <= i;  j++){
+                System.out.println("T");
+                    }
+                }
+            }
+        }
+    }
+
