22b00e5aa94fefbc644fe89cd1cbbd959e9ec46a
Mark Edzel P. Bacocanag <markbacocanag123@gmail.com>
2024-08-31 17:41:16 +0800

    Create SwingAlert.java

---
+++ Chapter(1)/SwingAlert.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+
+ public static void main(String[] args){
+      
+      JOptionPane.showMessageDialog(null, "Hello Word!", "Message", JOptionPane.INFORMATION_MESSAGE);
+ 
+ }
+
+}
