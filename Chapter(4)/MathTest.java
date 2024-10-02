9b32442eba7261aa64e87b5b632509b4503e54e3
Mark Edzel P. Bacocanag <markbacocanag123@gmail.com>
2024-10-02 17:07:22 +0800

    Exploring Java's Math Class

---
+++ Chapter(4)/MathTest.java
@@ -0,0 +1,17 @@
+public class MathTest {
+    public static void main(String[] args){
+     double x = 22.66;
+     double y = 3;
+
+        System.out.println("SQRT: " + Math.sqrt(x) );
+        System.out.println("SIN:  " + Math.sin(x));
+        System.out.println("COS: " + Math.cos(x));
+        System.out.println("FLOOR: " + Math.floor(x));
+        System.out.println("CEIL: " + Math.ceil(x));
+        System.out.println("ROUND: " + Math.round(x));
+        System.out.println("MAX: " + Math.max(x, y));
+        System.out.println("MIN: " + Math.min(x, y));
+        System.out.println("RANDOM: " + Math.random());
+
+    }
+}
