83e35616717928ad6cb107d82195ab8427771261
Mark Edzel P. Bacocanag <markbacocanag123@gmail.com>
2024-11-15 21:35:49 +0800

    Create Palindrome.java

---
+++ Chapter(7)/Palindrome.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Palindrome{
+
+   public static void main(String[] args){
+      String word;
+      
+      Scanner scn;
+      scn = new Scanner(System.in);
+      
+      System.out.println("Enter a word: ");
+      word = scn.nextLine();
+      scn.close();
+      
+      boolean result = isPalindrome(word);
+      
+      if(result == true){
+         
+         System.out.println("This " + word + " is Palindrome");
+      
+      }else {
+      
+         System.out.println("This " + word + " is not Palindarome");
+      
+      }
+     }
+      
+   public static boolean isPalindrome(String word){
+         
+         int left = 0;
+         int right = word.length() - 1; 
+         
+         while(left < right){
+         
+            if(word.charAt(left) != word.charAt(right)){
+            
+               return false;
+            
+            }
+           left++;
+           right--;
+         }  
+         return true;
+      }
+   }
